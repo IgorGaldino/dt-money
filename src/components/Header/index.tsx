0b4aa56bd86ba7958bbf268018349b236d561d45
Igor Galdino <igormgaldino@gmail.com>
2021-11-18 20:44:10 -0300

    Create application and Header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,15 @@
+import logoSvg from "../../assets/logo.svg";
+import { Container, Content } from "./styles";
+
+export function Header () {
+  return (
+    <Container>
+      <Content>
+        <img src={logoSvg} alt="dt money" />
+        <button type="button">
+          Nova transação
+        </button>
+      </Content>
+    </Container>
+  )
+}
