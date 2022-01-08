6826df4e658462aebf42acfd59a5644ef4b0d68d
Igor Galdino <igormgaldino@gmail.com>
2022-01-08 14:40:39 -0300

    Create transactions and list in table

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -3,6 +3,8 @@ import Modal from "react-modal";
 import incomeImg from "../../assets/income.svg";
 import outcomeImg from "../../assets/outcome.svg";
 import closeImg from "../../assets/close.svg";
+import { api } from "../../services/api";
+
 import { Container, RadioBox, TransactionTypeContainer } from "./styles";
 
 interface NewTransactionModalProps {
@@ -21,6 +23,15 @@ export function NewTransactionModal({
 
   function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault();
+
+    const data = {
+      title,
+      value,
+      category,
+      type
+    }
+
+    api.post("/transactions", data);
   }
 
   return (
